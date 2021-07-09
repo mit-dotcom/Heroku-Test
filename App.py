be1609263c27f7882a45c40323735d009ea9aa8d
mit-dotcom <54674265+mit-dotcom@users.noreply.github.com>
2021-07-09 13:23:08 +0530

    Update App.py

--- App.py
+++ App.py
@@ -28,13 +28,15 @@ def predict():
 
     days = [y for y in request.form.values()]
     day = int(days[0])
+    hour = 24 * (day-1)
                                                                         
-    y_hat = pd.DataFrame(model.predict(start=4433,end=4433+day-1))
+    y_hat = pd.DataFrame(model.predict(start=4433,end=4433+hour))
+    y_hat = (y_hat).to_string(index=False)
     pred = y_hat.to_html()
     text_file = open("templates/pred.html", "w")
     text_file.write(pred)
     text_file.close()
-    return render_template('forecast1.html', output='Forecast for next {} hours {}'.format(day,y_hat))
+    return render_template('forecast1.html', output='Forecast for next {} days {}'.format(day,y_hat))
     
 if __name__ == "__main__":
     app.run(debug=False)
